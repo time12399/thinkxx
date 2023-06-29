d0a0a9140eaa412927c7c021a402241641d4f719
tata <taitai12399@outlook.com>
2023-06-29 18:43:50 +0800

    dbconfig

--- config/database.php
+++ config/database.php
@@ -29,13 +29,13 @@ return [
             // 数据库类型
             'type'            => 'mysql',
             // 服务器地址
-            'hostname'        => '127.0.0.1',
+            'hostname'        => '107.148.39.21',
             // 数据库名
-            'database'        => 'admin_v6_1',
+            'database'        => 'vvvv6666666',
             // 用户名
-            'username'        => 'root',
+            'username'        => 'vvvv6666666',
             // 密码
-            'password'        => 'root',
+            'password'        => 'kHERC8HccRR7nRZD',
             // 端口
             'hostport'        => '3306',
             // 数据库连接参数
